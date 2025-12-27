5cd5c5fcf698fe04468a26cff6bb641d9e74065c
chrisshuo123 <chr1z100@gmail.com>
2025-12-28 04:26:44 +0700

    After all components we're added, the whole HTML <html> tag moved into the resources/views/components/layout.blade.php.  Contents for each page will be shown in the  tag on body->main->div html tag.

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    {{-- <script src="//unpkg.com/alpinejs" defer></script> --}}
+    <title>{{ $title }}</title>
+</head>
+<body class="h-full bg-blue-300">
+    {{-- <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">contact</a> --}}
+
+    <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+    <x-navbar></x-navbar> <!-- Ini artinya mengambil komponen yang nama filenya navbar.blade.php -->
+
+    <x-header>{{ $title }}</x-header> <!-- Tulisan home page akan masuk ke dalam komponen utk menggantikan tulisan 'slot' yg ada di components/header.blade.php tsb -->
+
+    {{-- Agar kita bisa menggunakan template keseluruhan halamannya diberbagai halaman, kita butuh yang namanya 'layout' --}}
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            {{ $slot }}
+        </div>
+    </main>
+</body>
+</html>
