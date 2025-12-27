1b73581ea4b22b24f8fc25dad9601197460c3c54
chrisshuo123 <chr1z100@gmail.com>
2025-12-28 04:19:59 +0700

    refine each page in resources/views to be simple after implement components (component commits will be done after this commit)

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,26 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Blog Page</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-    <a href="/about">About</a>
-    <a href="/contact">contact</a>
-    <h1>Halaman Blog</h1>
-    <article>
-        <h3>Mengapa Mr. Bean Lucu sampai tahun ini?</h3>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h1 class="text-5xl mb-2">Halaman Blog</h1>
+    <p class="mb-6">Welcome to my Blog!</p>
+    <article class="mb-4">
+        <h3 class="text-2xl">Mengapa Mr. Bean Lucu sampai tahun ini?</h3>
         <p>Karena Mr. Bean itu terlalu Wildd</p>
     </article>
 
-    <article>
-        <h3>Siapa Pengemudi Mobil Biru di film Mr. Bean?</h3>
+    <article class="mb-4">
+        <h3 class="text-2xl">Siapa Pengemudi Mobil Biru di film Mr. Bean?</h3>
         <p>Kemungkinan besar adalah tetangganya Mr. Bean yang paling ia benci</p>
     </article>
-</body>
-</html>
+</x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Contact Page</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-    <a href="/about">About</a>
-    <a href="/contact">contact</a>
-    <h1>Halaman Contact</h1>
+<x-layout>
+    <x-slot:title>Contact Page</x-slot:title>
+    <h1 class="text-5xl mb-4">Halaman Contact</h1>
     <ul>
-        <li>Email: {{ $email }}</li>
-        <li>Github: {{ $github }}</li>
+        <li><b>Email:</b>&nbsp;&nbsp; {{ $email }}</li>
+        <li><b>Github:</b>&nbsp;&nbsp; {{ $github }}</li>
     </ul>
-</body>
-</html>
+</x-layout>
