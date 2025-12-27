0ef6620ea0df2374f321940cebc2d7716ecceba1
chrisshuo123 <chr1z100@gmail.com>
2025-12-28 04:32:01 +0700

    Added route to blog and contact page. furthermore, we add the title array in each route to support the head and header title functionallity via x-slot:title on each views page, then passes onto layout.blade.php component page according to the page where we visit

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Chris Shuo']);
+    return view('about', ['nama' => 'Chris Shuo'], ['title' => 'About Page']);
 });
 
 // TUGAS: Buat 2 rute baru
@@ -18,9 +18,9 @@ Route::get('/about', function () {
 // ada email / social media
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog Page']);
 });
 
 Route::get('/contact', function () {
-    return view('contact', ['email' => 'shuo@email.com', 'github' => 'https://github.com/chrisshuo123']);
+    return view('contact', ['email' => 'shuo@email.com', 'github' => 'https://github.com/chrisshuo123'], ['title' => 'Contact Page']);
 });
