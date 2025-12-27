18ddc4c86a10d56ef835254256669ac49a976ba4
chrisshuo123 <chr1z100@gmail.com>
2025-12-28 04:19:59 +0700

    refine each page in resources/views to be simple after implement components (component commits will be done after this commit)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>About Page</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-    <a href="/about">About</a>
-    <a href="/contact">contact</a>
-    <h1>Halaman About</h1>
-    <h3>Owner: {{ $nama }}</h3>
+<x-layout>
+    <x-slot:title>About Page</x-slot:title>
+    <h1 class="text-5xl mb-2">Halaman About</h1>
+    <h3 class="text-3xl mb-6">Owner: {{ $nama }}</h3>
     <img src="img/mr-bean-funny-car.JPEG" style="width:50%;">
-</body>
-</html>
+</x-layout>
