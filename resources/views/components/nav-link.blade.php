e8b7eb95ffd9ab4442dd94b70f0d5cb0514edc91
chrisshuo123 <chr1z100@gmail.com>
2025-12-28 04:23:11 +0700

    this is where the nav button class we're functioned. If user clicks on a button, the nav button will be active, where deactivate the button before.

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,11 @@
+{{-- $attributes: Artinya, apapun yg ditulis didalam komponennya, itu akan di merge atau digabungkan kedalam componentnya, atau kedalam tag aslinya --}}
+{{-- $slot: Untuk judul per tombol navbarnya, dimasukkan kedalam $slot ini --}}
+
+{{-- <a {{ $attributes }} aria-current="page"
+class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+aria-current="{{ request()->is('/') ? 'page' : false }}">{{ $slot }}</a> --}}
+
+{{-- Pada bagian 'request()->is('/')' diatas, diganti menjadi '$active' --}}
+<a {{ $attributes }} aria-current="page"
+class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
