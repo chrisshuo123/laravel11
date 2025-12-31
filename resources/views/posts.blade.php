a5a1066281f87584b77200fd4ab1a166cda29f7b
chrisshuo123 <chr1z100@gmail.com>
2026-01-01 04:20:23 +0700

    refine the article layout into the @foreach (laravel's feature) loop

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,17 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
     
+    @foreach($posts as $post)
     <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
+        <a href="/posts/{{ $post['id'] }}" class="hover:underline">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">Chris Shuo</a> | 31 Desember 2025
+            <a href="#">{{ $post['author'] }}</a> | 31 Desember 2025
         </div>
-        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores est, voluptates, nam error quas beatae, odit eos voluptate veniam recusandae numquam atque quidem at sint repellat. Perspiciatis debitis quia ab.</p>
-        <a class="font-medium text-blue-500 hover:underline" href="#">Read More &raquo;</a>
+        <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
+        <a href="/posts/{{ $post['id'] }}" class="font-medium text-blue-500 hover:underline" href="#">Read More &raquo;</a>
     </article>
-    <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
-        <div class="text-base text-gray-500">
-            <a href="#">Chris Shuo</a> | 1 Januari 2026
-        </div>
-        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem laudantium vero, repellat ipsam neque consequatur eveniet tenetur vitae sint ipsum quibusdam ex quod error! Maxime ipsum ex distinctio corporis officia.</p>
-        <a class="font-medium text-blue-500 hover:underline" href="#">Read More &raquo;</a>
-    </article>
+    @endforeach
     {{-- <h1 class="text-5xl mb-2">Halaman Blog</h1>
     <p class="mb-6">Welcome to my Blog!</p>
     <article class="mb-4">
