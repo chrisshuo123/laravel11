973c66486574ffb3117e9396310bf8d292459faa
chrisshuo123 <chr1z100@gmail.com>
2026-01-01 03:16:08 +0700

    modify the Blog layout with refined article

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Chris Shuo</a> | 31 Desember 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores est, voluptates, nam error quas beatae, odit eos voluptate veniam recusandae numquam atque quidem at sint repellat. Perspiciatis debitis quia ab.</p>
+        <a class="font-medium text-blue-500 hover:underline" href="#">Read More &raquo;</a>
+    </article>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Chris Shuo</a> | 1 Januari 2026
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem laudantium vero, repellat ipsam neque consequatur eveniet tenetur vitae sint ipsum quibusdam ex quod error! Maxime ipsum ex distinctio corporis officia.</p>
+        <a class="font-medium text-blue-500 hover:underline" href="#">Read More &raquo;</a>
+    </article>
+    {{-- <h1 class="text-5xl mb-2">Halaman Blog</h1>
+    <p class="mb-6">Welcome to my Blog!</p>
+    <article class="mb-4">
+        <h3 class="text-2xl">Mengapa Mr. Bean Lucu sampai tahun ini?</h3>
+        <p>Karena Mr. Bean itu terlalu Wildd</p>
+    </article>
+
+    <article class="mb-4">
+        <h3 class="text-2xl">Siapa Pengemudi Mobil Biru di film Mr. Bean?</h3>
+        <p>Kemungkinan besar adalah tetangganya Mr. Bean yang paling ia benci</p>
+    </article>  --}}
+</x-layout>
