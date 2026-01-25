f754a8f9d7f520c49e2fcff2f43fe75699302604
chrisshuo123 <chr1z100@gmail.com>
2026-01-26 01:35:26 +0700

    add label command that distinguish the difference between Blog page and Posts page in routes/web.php on Route::get /posts and posts/{slug}

--- routes/web.php
+++ routes/web.php
@@ -12,34 +12,40 @@ Route::get('/about', function () {
     return view('about', ['nama' => 'Chris Shuo', 'title' => 'About Page']);
 });
 
+// Di halaman Blog Utama
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog Page', 'posts' => [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Chris Shuo',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores est, voluptates, nam error quas beatae, odit eos voluptate veniam recusandae numquam atque quidem at sint repellat. Perspiciatis debitis quia ab.'
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
-            'author' => 'Suho Sou',
+            'author' => 'Suho Soull',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem laudantium vero, repellat ipsam neque consequatur eveniet tenetur vitae sint ipsum quibusdam ex quod error! Maxime ipsum ex distinctio corporis officia.'
         ]
     ]]);
 });
 
-Route::get('posts/{id}', function($id) {
+// Di hal. Posts, pada hal. Blog kita pilih
+Route::get('posts/{slug}', function($slug) {
     // dd($id);
     $posts = [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Chris Shuo',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores est, voluptates, nam error quas beatae, odit eos voluptate veniam recusandae numquam atque quidem at sint repellat. Perspiciatis debitis quia ab.'
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Suho Sou',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem laudantium vero, repellat ipsam neque consequatur eveniet tenetur vitae sint ipsum quibusdam ex quod error! Maxime ipsum ex distinctio corporis officia.'
@@ -48,8 +54,8 @@ Route::get('posts/{id}', function($id) {
 
     // Ingat: Var yg ditulis didlm sebuah function itu hanya bisa dipakai didalam function itu
     // Spy kita bisa gunakan, di PHP 8 bisa pakai use seperti dibawah ini. Dgn ini ia bisa ngecek globalnya
-    $post = Arr::first($posts, function($post) use ($id) {
-        return $post['id'] == $id;
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
     });
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
