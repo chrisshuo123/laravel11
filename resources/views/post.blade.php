018042c2828e549eadca17480fb9178646445b38
chrisshuo123 <chr1z100@gmail.com>
2026-02-08 23:31:34 +0700

    change the template date of posts to be taken from the 'created_at' column from table posts, with 'diffForHumans()' for user ease of UI Experience

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,11 @@
     <article class="py-8 max-w-screen-md">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 31 Desember 2025
+            <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
+            {{-- Utk waktu bisa pakai:
+            $post->created_at->format('j F Y')
+            atau
+            $post->created_at->diffForHumans --}}
         </div>
         <p class="my-4 font-light">{{ $post['body'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to Posts</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,11 @@
         <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 31 Desember 2025
+            <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
+            {{-- Utk waktu bisa pakai:
+            $post->created_at->format('j F Y')
+            atau
+            $post->created_at->diffForHumans --}}
         </div>
         <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
         <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline" >Read More &raquo;</a>
