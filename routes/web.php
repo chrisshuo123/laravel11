14a0e1f2add92dc0cb470b3a0b59194dddb60864
chrisshuo123 <chr1z100@gmail.com>
2026-02-08 22:39:15 +0700

    change back post_id pk into slug, and in the routes/web.php, we twitch the Route posts with 'Route Model Binding' technique

--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,18 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-
 Route::get('/about', function () {
     return view('about', ['nama' => 'Chris Shuo', 'title' => 'About Page']);
 });
 
-// Route Blog Posts, sekaligus mempelajari tentang penerapan "Wild Card"
-// Di halaman Blog Utama
-// Pada branch 'part4_model', seluruh array posts dipindah ke dalam class Post dengan method all
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
 });
 
-// Di hal. Posts, pada hal. Blog kita pilih
-Route::get('posts/{slug}', function($slug) {
-    // Pada branch 'part4_model', param dgn $posts dirubah ke 'Post::all'. Panggil class dan methodnya.  var $post telah dipindah ke dalam Models\Post.php, karna hal2 yg berhubungan dgn ngerubah isi (kyk id jadi slug), harus dilakukan pada model scr prinsip.
-    $post = Post::find($slug);
-
+// Pada posts ini, kita menerapkan cara "Route Model Binding" yang dapat ditemui pada Laravel Docs.
+Route::get('/posts/{post:slug}', function(Post $post) { // Kembali pakai slug lagi
+    
     return view('post', ['title' => 'Single Post', 'post' => $post]);
-
-    // dd($post);
 });
 
 Route::get('/contact', function () {
