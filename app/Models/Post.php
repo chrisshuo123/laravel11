6da691fcbb08283ecabc05654b43d44b16755808
chrisshuo123 <chr1z100@gmail.com>
2026-02-08 23:23:28 +0700

    Tambah protected fillable untuk column table posts yang dapat diisi, disertakan dengan penjelasan menggunakan command //

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
+// 1. Set protected fillable untuk menyertakan column mana saja yang boleh diisi.  Kalau fillable tidak diisi, maka bisa terkena error MassAssignmentException.  Untuk detailnya, bisa dilihat pada docs laravel 'Mass Assignment'
 class Post extends Model {
-    protected $table = 'blog_posts'; // jika contoh nama table nya 'blog_posts', maka tambahkan protected var $table = nama tabel yang sesuai.
-    protected $primaryKey = 'post_id'; // jika contoh PK nya bukan id, tetapi post_id, maka harus disertakan var protected primaryKey tersebut.
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
+
+// 2. Pada terminal, input:
+// php artisan tinker
+/**
+ * App\Model\Post::create([         << klik 'enter'.  Akan membuat line baru selama didlm array.
+ * 'title' => 'Judul Artikel 1',
+ * 'author' => 'Chris Shuo',
+ * 'slug' => 'judul-artikel-1',
+ * 'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dicta quasi nam nobis consequatur, eligendi temporibus doloremque quae! Debitis a error deleniti modi illum cumque obcaecati ducimus. Magnam, exercitationem perferendis?'
+ * ]);              << Sampai disini, klik enter maka jika tak ada MassAssignmentException Error maka data sudah terinsert ke table posts.
+ */
