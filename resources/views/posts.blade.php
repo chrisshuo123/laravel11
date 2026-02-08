2e436435e315fe736bc409121a5c056297daddb2
chrisshuo123 <chr1z100@gmail.com>
2026-02-08 22:09:25 +0700

    change slug into post_id (pk table in blog_posts previously posts), where eloquent does not support slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,13 +3,13 @@
     
     @foreach($posts as $post)
     <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+        <a href="/posts/{{ $post['post_id'] }}" class="hover:underline">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
             <a href="#">{{ $post['author'] }}</a> | 31 Desember 2025
         </div>
         <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
-        <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline" >Read More &raquo;</a>
+        <a href="/posts/{{ $post['post_id'] }}" class="font-medium text-blue-500 hover:underline" >Read More &raquo;</a>
     </article>
     @endforeach
     {{-- <h1 class="text-5xl mb-2">Halaman Blog</h1>
