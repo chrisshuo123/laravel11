4a9719c504fc8dac4a5ee1807ef9bd0ced484d86
chrisshuo123 <chr1z100@gmail.com>
2026-07-24 03:38:49 +0700

    make changes in the posts.blade.php h2 title, while add .gitignore for the stubs directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,8 @@
     @foreach($posts as $post)
     <article class="py-8 max-w-screen-md border-b border-gray-300">
         <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+        </a>
         <div class="text-base text-gray-500">
             <a href="#">{{ $post['author'] }}</a> | 31 Desember 2025
         </div>
